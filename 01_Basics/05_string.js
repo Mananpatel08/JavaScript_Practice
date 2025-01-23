3156b03e40e6b1d4f9507eade1095a58b91e2616
Manan patel <160469109+Mananpatel08@users.noreply.github.com>
2025-01-23 07:27:51 +0000

    array & string

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,7 @@
+const name = "Manan"
+const age = 21
+
+console.log(`Hello Gyus my name is ${name}, and my age is ${age}`)
+
+console.log(name[0])
+console.log(name.length)
---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,9 @@
+//array
+const myArr = [ 0 , 1 , 2 , 4 , "manan" , true ]
+
+console.log(myArr[0])
+
+//methods
+
+myArr.push(1)       // insert at last
+myArr.pop()         // remove last element
