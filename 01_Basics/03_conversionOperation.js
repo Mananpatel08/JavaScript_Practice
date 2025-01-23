54e14c06868c6b5e3284ca62624d6cbbcb662561
Manan patel <160469109+Mananpatel08@users.noreply.github.com>
2025-01-23 05:41:26 +0000

    operations & compairison done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 // string => number
 let score = "20abc"
 
-console.log(typeof(score))                  // string
+// console.log(typeof(score))                  // string
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))          // Number
-console.log(valueInNumber)                  // NaN ( not a number )
+// console.log(typeof(valueInNumber))          // Number
+// console.log(valueInNumber)                  // NaN ( not a number )
 
 /*
 note:
@@ -18,7 +18,7 @@ true => 1 , false => 0
 // string => boolean
 let isLoggedIn = "manan"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof(booleanIsLoggedIn))
+// console.log(typeof(booleanIsLoggedIn))
 
 /*
 node:
@@ -29,5 +29,29 @@ node:
 // number => string
 let num = 10
 let numberIsnum = String(num)
-console.log(numberIsnum)              //10
-console.log(typeof(numberIsnum))      //String
+// console.log(numberIsnum)              //10
+// console.log(typeof(numberIsnum))     //String
+
+
+// *********************************** Operations ****************************************
+
+let value = 3
+let nagValue = -(value)
+// console.log(nagValue)                   // -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);          //2 to power 3
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "ok"
+let str2 = "bye"
+
+// console.log( str1 + str2 )      // okbye
+
+console.log( 2 + "2" );             //22
+console.log( "2" + 2 );            //22
+console.log( "2" + 2 + 1 );         //221
+console.log( 2 + 2 + "4" );         //44
---
+++ 01_Basics/04_compairison.js
@@ -0,0 +1,15 @@
+// console.log(4>12);
+// console.log(4<12);
+// console.log(4>=12);
+// console.log(12 == 12);
+// console.log(12 != 12);
+
+// console.log( null > 0 );        //false
+// console.log( null == 0 );       //false
+// console.log( null >= 0 );       //true
+// becaus it conver null value into zero
+
+
+// === ( for strict check it check datatype and value both )
+console.log( "2" == 2 );       // true
+console.log( "2" === 2 );       // false
